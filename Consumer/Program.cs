e378dffff73782e421150aa48a96c277baf1e2ec
Jorge Hernandes <jorgehernandes@gmail.com>
2019-11-16 21:21:52 -0300

    commit inicial

---
+++ Consumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            { 
+                HostName = "localhost",
+                Port = 5672,
+                VirtualHost = "dev",
+                UserName = "jorge",
+                Password = "senha",
+                AutomaticRecoveryEnabled = true,
+                NetworkRecoveryInterval = TimeSpan.FromSeconds(3)
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var correlationId = ea.BasicProperties.CorrelationId;
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($" [x] Received {message} - CorrelationId {correlationId}");
+
+                    channel.BasicAck(ea.DeliveryTag, false);
+                };
+
+                channel.BasicConsume(queue: "test.queue",
+                                    autoAck: false,
+                                    consumer: consumer);
+                
+                Thread.Sleep(10000);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            { 
+                HostName = "localhost",
+                Port = 5672,
+                VirtualHost = "dev",
+                UserName = "jorge",
+                Password = "senha",
+                AutomaticRecoveryEnabled = true,
+                NetworkRecoveryInterval = TimeSpan.FromSeconds(3)
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ConfirmSelect();
+
+                string correlationId = Guid.NewGuid().ToString();
+
+                var messageProperties = channel.CreateBasicProperties();
+                messageProperties.Persistent = true;
+                messageProperties.CorrelationId = correlationId;
+                messageProperties.ContentType = "application/json";
+
+                string message = @"{""message"":""Hello World!""}";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: "test.exchange",
+                    routingKey: "",
+                    basicProperties: messageProperties,
+                    body: body
+                );
+
+                channel.WaitForConfirmsOrDie(TimeSpan.FromSeconds(5));
+
+                Console.WriteLine($" [x] Sent {message} - CorrelationId {correlationId}");
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
